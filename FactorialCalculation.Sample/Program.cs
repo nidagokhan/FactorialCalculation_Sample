9808ced3c23ff92882038c76fd1799072f873e56
ndgkhn <nida.gokhan@gmail.com>
2023-08-10 23:29:36 +0300

    Add project files.

---
+++ FactorialCalculation.Sample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialCalculation.Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number!");
+            int number=int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(number));
+
+            Console.ReadKey();
+        }
+
+        public static int Factorial(int a)
+        {
+            if (a == 1)
+            {
+                return 1;
+            }
+            else
+            {
+               return a*Factorial(a-1);
+            }
+        }
+    }
+}
